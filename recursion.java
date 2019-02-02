b670963fb146d98a4fc994cf6e0aa5adc9f5d880
Minuk Kim <mkim01@stuy.edu>
2019-02-02 14:57:50 -0500

    fib(n) method and testing main method

--- recursion.java
+++ recursion.java
@@ -7,19 +7,37 @@ public class recursion{
 
     */
     public static double sqrt(double n, double tolerance){
-
+      return 0.0;
     }
 
     /*Recursively find the n'th fibbonaci number in linear time
      *fib(0) = 1; fib(1) = 1; fib(5) = 5
      *precondition: n is non-negative
      */
+     // 1 1 2 3 5 8 13 21 34
     public static int fib(int n){
-
+      if (n == 0){
+        return 1;
+      }
+      else if (n == 1){
+        return 1;
+      }
+      else {
+        return fib(n - 2) + fib(n - 1);
+      }
     }
 
     /*As Per classwork*/
-    public static ArrayList<Integer> makeAllSums(){
+    // public static ArrayList<Integer> makeAllSums(){
+    //   ArrayList<Integer> a = new ArrayList<Integer>();
+    //   return a;
+    // }
+
+
+    public static void main (String args[]){
+      for(int i = 0; i < 10; i++){
+        System.out.println(fib(i));
+      }
     }
 
 }
