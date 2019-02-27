273dd1db3ca959051567f79ebc6d79f2feb0fdba
Minuk Kim <mkim01@stuy.edu>
2019-02-27 04:49:49 -0500

    mr.k's test cases

--- recursion.java
+++ recursion.java
@@ -19,12 +19,13 @@ public class recursion{
     }
 
     private static double approx(double n, double guess, double tolerance){
+      if (n == 0){
+        return 0;
+      }
       if (check(n, guess, tolerance)){
         return guess;
       }
-      else{
         return approx(n, (n / guess + guess) / 2, tolerance);
-      }
     }
 
     public static double sqrt(double n, double tolerance){
@@ -84,20 +85,88 @@ public class recursion{
       }
     }
 
+    public static boolean closeEnough(double a, double b){
+      if(a==0.0 && b==0.0)return true;
+      if(a==0.0)return b < 0.00000000001;
+      if(b==0.0)return a < 0.00000000001;
+      return Math.abs(a-b)/a < 0.0001;//This is the normal % difference allowed
+    }
 
-    public static void main (String args[]){
-      for(int i = 1; i < 10; i++){
-          System.out.println("fibbonaci number(" + i + "):");
-          System.out.println(fib(i));
+    public static void testFib(int testcase){
+    recursion r = new recursion();
+    int[] input = {0,1,2,3,5,30};
+    int[] output ={0,1,1,2,5,832040};
+    int max = input.length;
+    if(testcase < input.length){
+      int in = input[testcase];
+      try{
+
+        int ans = r.fib(in);
+        int correct = output[testcase];
+        if(ans == correct){
+          System.out.println("PASS test fib "+in+". "+correct);
+        }
+        else{
+          System.out.println("FAIL test fib"+in+". "+ans+" vs "+correct);
+
+        }
+      }catch(IllegalArgumentException n){
+        if(in < 0){
+          System.out.println("PASS test fib"+in+" IllegalArgumentException");
+        }else{
+          System.out.println(" FAIL IllegalArgumentException in test case:"+in);
+        }
+      }catch(Exception e){
+        System.out.println(" FAIL Some exception in test case:"+in);
+    }
+  }
+}
+  public static void testSqrt(int testcase){
+    recursion r = new recursion();
+    double[] input = {0.0,1.0, 2.0, 4.0, 7.0};
+    double[] output = {0.0,1.0,1.4142135623730951,2.0,2.6457513110645907};
+    int max = input.length;
+    if(testcase < input.length){
+    double in = input[testcase];
+    try{
+    double ans = r.sqrt(in,.00001);
+    double correct = Math.sqrt(in);
+    if(closeEnough(ans,correct)){
+      System.out.println("PASS test sqrt "+in+" "+ans);
       }
-        System.out.println("sqare root of 2: " + Math.sqrt(2));
-         System.out.println("approximation with tolerance of 0.001%" + "" + ": "+ sqrt(2.0, 1));
-        System.out.println("sqare root of 3: " + Math.sqrt(3));
-         System.out.println("sqare root of 3 with tolerance of 0.001%" + "" + ": "+ sqrt(3.0, 1));
+    else{
+      System.out.println("FAIL test sqrt "+in+" "+ans+" vs "+correct);
 
-
-      for (int i = 0; i < 5; i++){
-          System.out.println("all possible sums from 1 to" + i + makeAllSums(i));
+      }
+    }catch(IllegalArgumentException n){
+    if(in < 0){
+      System.out.println("PASS test sqrt"+in+" IllegalArgumentException");
+    }else{
+      System.out.println(" FAIL IllegalArgumentException in test case:"+in);
+      }
+    }catch(Exception e){
+    System.out.println(" FAIL Some exception in test case:"+in);
       }
     }
   }
+
+    public static void main (String args[]){
+      for (int i = 0; i < 6; i++){
+        testFib(i);
+        testSqrt(i);
+        }
+      // for(int i = 1; i < 10; i++){
+      //     System.out.println("fibbonaci number(" + i + "):");
+      //     System.out.println(fib(i));
+      // }
+      //   System.out.println("sqare root of 2: " + Math.sqrt(2));
+      //    System.out.println("approximation with tolerance of 0.001%" + "" + ": "+ sqrt(2.0, 1));
+      //   System.out.println("sqare root of 3: " + Math.sqrt(3));
+      //    System.out.println("sqare root of 3 with tolerance of 0.001%" + "" + ": "+ sqrt(3.0, 1));
+      //
+      //
+      // for (int i = 0; i < 5; i++){
+      //     System.out.println("all possible sums from 1 to" + i + makeAllSums(i));
+      // }
+  }
+}
